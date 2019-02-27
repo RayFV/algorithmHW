10a7a8a1c730e80c81952f35a91b385177e9d7f9
RayFV <rayfvfree@outlook.com>
2019-02-27 15:18:29 +0800

    Update HW01_B10423056.py

--- HW1/HW01_B10423056.py
+++ HW1/HW01_B10423056.py
@@ -1,23 +1,24 @@
-str = input("請輸入中文字串：")
-#str = "花世界"
+userInput = input("請輸入中文字串：")
+#userInput = "花世界"
 
 # mostLength=1
-# for i in range(1, len(str) + 1):
+# for i in range(1, len(userInput) + 1):
 #     mostLength *= i
 
-def test(list):
+def permutations(list):
     result = []
+    if len(list) <= 1:
+        return list
     if len(list) == 2:
         result.append(list)
         temp = [list[1], list[0]]
         listAppendWithoutDuplicate(result, temp)
     elif len(list) >= 3:
         for i in range(len(list)):
-            for j in test(list[1:]):
+            list[0], list[i] = list[i], list[0]
+            for j in permutations(list[1:]):
                 temp = list[:1] + j
                 listAppendWithoutDuplicate(result, temp)
-            if(i != len(list) - 1):
-                list[0], list[i+1] = list[i+1], list[0]
             #print(result)
     return result
 
@@ -25,12 +26,11 @@ def listAppendWithoutDuplicate(originalList, newItem):
     if newItem not in originalList:
         originalList.append(newItem)
 
-
 def displayResult(result):
     for i, item in enumerate(result):
         print("{0}. {1}".format(i+1, ''.join(item)))
     
     print("共{0}種文字組合".format(len(result)))
 
-strList = list(str)
-displayResult(test(strList))
+strList = list(userInput)
+displayResult(permutations(strList))
