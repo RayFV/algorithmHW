22524883c33409ccb8b4a437ece9c17cd180959d
RayFV <rayfvfree@outlook.com>
2019-02-27 14:58:44 +0800

    Update HW01_B10423056.py

--- HW1/HW01_B10423056.py
+++ HW1/HW01_B10423056.py
@@ -1,34 +1,36 @@
-#str = input("請輸入中文字串：")
-str = "0012"
+str = input("請輸入中文字串：")
+#str = "花世界"
 
-strLen = len(str)
-
-mostLength=1
-for i in range(1, strLen + 1):
-    mostLength *= i
-
-allList = [[0] * strLen for _ in range(mostLength)]
+# mostLength=1
+# for i in range(1, len(str) + 1):
+#     mostLength *= i
 
 def test(list):
     result = []
     if len(list) == 2:
         result.append(list)
-        swap = [list[1], list[0]]
-        result.append(swap)
+        temp = [list[1], list[0]]
+        listAppendWithoutDuplicate(result, temp)
     elif len(list) >= 3:
         for i in range(len(list)):
             for j in test(list[1:]):
                 temp = list[:1] + j
-                if temp not in result:
-                    result.append(temp)
+                listAppendWithoutDuplicate(result, temp)
             if(i != len(list) - 1):
                 list[0], list[i+1] = list[i+1], list[0]
-            
-            print(result)
+            #print(result)
     return result
 
-strList = list(str)
-result = test(strList)
+def listAppendWithoutDuplicate(originalList, newItem):
+    if newItem not in originalList:
+        originalList.append(newItem)
 
-print("length", len(result))
-print(result)
+
+def displayResult(result):
+    for i, item in enumerate(result):
+        print("{0}. {1}".format(i+1, ''.join(item)))
+    
+    print("共{0}種文字組合".format(len(result)))
+
+strList = list(str)
+displayResult(test(strList))
