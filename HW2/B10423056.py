8a736af462e0fb81b1f0e7a81bba757479f02ca1
Raymond <rayfvfree@outlook.com>
2019-04-06 15:56:15 +0800

    Refactor
    移除與作業不相關的（除了BFS

--- HW2/B10423056.py
+++ HW2/B10423056.py
@@ -5,75 +5,81 @@
 做法：
 先把輸入的中序轉成後序，再建成二元樹。
 然後可以列印出中序、後序、前序，並且可以運算出答案
+
+輸出樹狀結構圖的部分需要用到Graphviz2.38
 '''
 
 import re
 import os
 
-# userInput = input("input:")
-userInput = '(((8+11)-(6*4))/((20-15)*6))'
-
-separators = re.compile(r'([+\-*/()])')
-operator = {'(': 0, '+': 5, '-': 5, '*': 10, '/': 10}
-
-postfixStack = []
-
-# 用separators切割并保留，這是考慮到有小數點的可能而使用的切割法
-infix = list(filter(None, separators.split(userInput)))
+OPERATOR = {'(': 0, '+': 5, '-': 5, '*': 10, '/': 10}
 
 
-class Tree:
+class Node:
+    id_count = 0
+
     def __init__(self, data=None):
-        self.bfsId = None
+        self.id = str(Node.id_count)
+        Node.id_count += 1
         self.data = data
         self.left = None
         self.right = None
-        self.countLeftChildren = 0
-        self.countRightChildren = 0
-        self.depth = 1
-
-    def isNumber(self):
-        return self.data.isdigit()
 
     def isOperator(self):
-        return self.data in operator
+        return self.data in OPERATOR
 
     def getInfix(self):
-        result = ""
-        if self.left is not None:
-            result = result + '(' + self.left.getInfix()
-        result = result + ' ' + self.data + ' '
-        if self.right is not None:
-            result = result + self.right.getInfix() + ')'
+        result = []
+        if self.left:
+            result.append('(')
+            result = result + self.left.getInfix()
+        result.append(self)
+        if self.right:
+            result = result + self.right.getInfix()
+            result.append(')')
         return result
 
-    def getPostFix(self):
-        result = ""
-        if self.left is not None:
-            result += self.left.getPostFix()
-        if self.right is not None:
-            result += self.right.getPostFix()
-        result = result + self.data + ' '
+    def getInfixText(self):
+        infix = self.getInfix()
+        result = " ".join(
+            [node.data if node not in ['(', ')'] else node for node in infix])
+        return result
+
+    def getPostfix(self):
+        result = []
+        if self.left:
+            result = result + self.left.getPostfix()
+        if self.right:
+            result = result + self.right.getPostfix()
+        result.append(self)
+        return result
+
+    def getPostfixText(self):
+        postfix = self.getPostfix()
+        result = " ".join([node.data for node in postfix])
         return result
 
     def getPrefix(self):
-        result = ""
-        result = result + self.data + ' '
-        if self.left is not None:
-            result += self.left.getPrefix()
-        if self.right is not None:
-            result += self.right.getPrefix()
+        result = []
+        result.append(self)
+        if self.left:
+            result = result + self.left.getPrefix()
+        if self.right:
+            result = result + self.right.getPrefix()
+        return result
+
+    def getPrefixText(self):
+        prefix = self.getPrefix()
+        result = " ".join([node.data for node in prefix])
         return result
 
     def getResult(self):
         if not self.isOperator:
             return
-
-        prefix = self.getPrefix().strip().split(' ')
+        prefix = self.getPrefixText().strip().split(' ')
         tempStack = []
-
         while(prefix):
-            if(prefix[-1] in operator):
+            if(prefix[-1] in OPERATOR):
                 op = prefix.pop()
                 a = float(tempStack.pop())
                 b = float(tempStack.pop())
@@ -87,12 +93,9 @@ class Tree:
                     tempStack.append(a / b)
             else:
                 tempStack.append(prefix.pop())
-
         return tempStack.pop()
 
-    def getTotalChild(self):
-        return self.countLeftChildren + self.countRightChildren
-
+    # BFS練習
     def getBFS(self):
         bfsResult = []
         queue = []
@@ -101,7 +104,6 @@ class Tree:
         while(queue):
             current = queue.pop(0)
             bfsResult.append(current)
-            current.bfsId = str(id_count)
             id_count += 1
             if(current.isOperator()):
                 queue.append(current.left)
@@ -109,7 +111,8 @@ class Tree:
         return bfsResult
 
 
-def infixToPostfix():
+def infixToPostfix(infix):
+    postfixStack = []
     tempStack = []
     for item in infix:
         if item == '(':
@@ -119,8 +122,8 @@ def infixToPostfix():
             while(tempItem != '('):
                 postfixStack.append(tempItem)
                 tempItem = tempStack.pop()
-        elif item in operator:
-            while(tempStack and operator[item] <= operator[tempStack[-1]]):
+        elif item in OPERATOR:
+            while(tempStack and OPERATOR[item] <= OPERATOR[tempStack[-1]]):
                 postfixStack.append(tempStack.pop())
             tempStack.append(item)
         else:
@@ -129,42 +132,39 @@ def infixToPostfix():
     while(tempStack):
         postfixStack.append(tempStack.pop())
 
+    return postfixStack
 
-def createTreeWithPosfix(current):
-    current.data = postfixStack.pop()
 
-    if not postfixStack or not current.isOperator():
+def createTreeWithPosfix(currentNode, postfixStack):
+    currentNode.data = postfixStack.pop()
+
+    if not postfixStack or not currentNode.isOperator():
         return
 
-    current.right = Tree()
-    current.right.depth += current.depth
-    current.countRightChildren += 1
-    createTreeWithPosfix(current.right)
-    current.countRightChildren += current.right.getTotalChild()
+    currentNode.right = Node()
+    createTreeWithPosfix(currentNode.right, postfixStack)
 
-    current.left = Tree()
-    current.left.depth += current.depth
-    current.countLeftChildren += 1
-    createTreeWithPosfix(current.left)
-    current.countLeftChildren += current.left.getTotalChild()
+    currentNode.left = Node()
+    createTreeWithPosfix(currentNode.left, postfixStack)
 
 
 # Create tree structure graph with graphviz
 def generateTreeGraph(root):
     treeStringResult = 'graph {\n  size="7,5";\n  node [color=goldenrod2, style=filled];\n  '
-    bfs = root.getBFS()
+
+    bfs = root.getBFS()  # 不一定需要使用BFS
 
     for node in bfs:
         # create label with unique id
-        treeStringResult += node.bfsId + ' [label="' + node.data + '"];\n  '
+        treeStringResult += node.id + ' [label="' + node.data + '"];\n  '
 
         # create Vertices and Edge
-        if node.left is not None:
-            treeStringResult += node.bfsId + ' -- ' + node.left.bfsId + ';\n  '
-        if node.right is not None:
-            treeStringResult += node.bfsId + ' -- ' + node.right.bfsId + ';\n  '
+        if node.left:
+            treeStringResult += node.id + ' -- ' + node.left.id + ';\n  '
+        if node.right:
+            treeStringResult += node.id + ' -- ' + node.right.id + ';\n  '
 
-    treeStringResult += "\n}"  # Close
+    treeStringResult = treeStringResult.rstrip() + "\n}"  # Close
 
     # Write to file
     fileName = 'treeGraph.dot'
@@ -179,22 +179,30 @@ def generateTreeGraph(root):
 
     print("Opening image...")
     os.system("treeGraph.png")
+    print("DONE. \n\n")
 
     # print(treeStringResult)
 
 
-infixToPostfix()
-# print("InfixList:", infix)
-# print("PostfixStack:", postfixStack)
+userInput = input("請輸入中序算術式: ").replace(" ", "")
+# userInput = '(((8+11)-(6*4))/((20-15)*6))'
 
-root = Tree()
-createTreeWithPosfix(root)
+separators = re.compile(r'([+\-*/()])')
 
-print("\nInfix: ", root.getInfix(), end="\n\n")
+# 用separators切割并保留，這是考慮到有小數點的可能而使用的切割法
+infix = list(filter(None, separators.split(userInput)))
+print(infix)
 
-print("Postfix: ", root.getPostFix(), end="\n\n")
+postfixStack = infixToPostfix(infix)
 
-print("Prefix: ", root.getPrefix(), end="\n\n")
+root = Node()  # Crete root
+createTreeWithPosfix(root, postfixStack)
+
+print("\nInfix: ", root.getInfixText(), end="\n\n")
+
+print("Postfix: ", root.getPostfixText(), end="\n\n")
+
+print("Prefix: ", root.getPrefixText(), end="\n\n")
 
 print("Result: ", root.getResult(), end="\n\n")
 
